a17ee8f772d465bab3adb21ac3485b57ab80c684
Tristyn <tristynstimpson@gmail.com>
2022-02-18 18:57:29 +0300

    apr and estimated daily reward

--- tools/create_distribution.ts
+++ tools/create_distribution.ts
@@ -1,16 +1,17 @@
+import { AssociatedToken } from "./../src/common/associatedToken"
+import { StakeClient } from "./../src/staking/client"
+import { StakePool } from "./../src/staking/stakePool"
+import { Distribution } from "./../src/rewards/distribution"
 import { RewardsClient } from "./../src/rewards/client"
 import * as anchor from "@project-serum/anchor"
-import { AccountLayout, TOKEN_PROGRAM_ID } from "@solana/spl-token"
-import { clusterApiUrl, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY } from "@solana/web3.js"
 import { resolve } from "path"
 import { homedir } from "os"
 import { Provider } from "@project-serum/anchor"
 
 const args = {
   cluster: "https://api.devnet.solana.com",
-  seed: "moose2",
-  target: "CTP5Rep8XYcoPBjHJY3nCG2yAxRvM2c3n9sHkLhjxebJ",
-  tokenSource: "5JKXeqsDCctvsXhzRBFY8VDVS3eDHbhTi9LAkcLxdPG3",
+  seed: "moose5",
+  targetStakePoolSeed: StakePool.CANONICAL_SEED,
   /** Distrubtion amount. Unit is lamports */
   amount: 1_000_000_000_000,
   /** Duration. Unit is days */
@@ -43,49 +44,33 @@ export interface DistributionConfig {
 async function main() {
   process.env.ANCHOR_WALLET = resolve(homedir(), ".config/solana/id.json")
   const provider = Provider.local(args.cluster)
+  const { connection, wallet } = provider
 
-  const wallet = provider.wallet
   const rewardsProgram = await RewardsClient.connect(provider)
+  const stakeProgram = await StakeClient.connect(provider)
+  const stakePool = await StakePool.load(stakeProgram, args.targetStakePoolSeed)
+  const tokenSource = await AssociatedToken.load(connection, stakePool.stakePool.tokenMint, wallet.publicKey)
 
-  const [distAccount, bumpSeed] = await PublicKey.findProgramAddress([Buffer.from(args.seed)], rewardsProgram.programId)
-
-  const [distVault, vaultBumpSeed] = await PublicKey.findProgramAddress(
-    [distAccount.toBuffer(), Buffer.from("vault")],
-    rewardsProgram.programId
-  )
+  if (!tokenSource) {
+    throw new Error("Wallet does not have a token account to withdraw from.")
+  }
 
   const now = Date.now() / 1000
   const end = now + 86400 * args.duration
 
-  const fundingSource = new PublicKey(args.tokenSource)
-  const fundingData = (await provider.connection.getAccountInfo(fundingSource)).data
-  const fundingInfo = AccountLayout.decode(fundingData)
-
-  await rewardsProgram.rpc.distributionCreate(
-    {
+  await Distribution.createForStakePool(rewardsProgram, stakePool, {
+    args: {
       amount: new anchor.BN(args.amount),
       authority: wallet.publicKey,
-      bumpSeed: bumpSeed,
       seed: args.seed,
-      targetAccount: new PublicKey(args.target),
-      vaultBump: vaultBumpSeed,
       beginAt: new anchor.BN(now),
       endAt: new anchor.BN(end)
     },
-    {
-      accounts: {
-        distribution: distAccount,
-        vault: distVault,
-        payerRent: wallet.publicKey,
-        payerTokenAuthority: wallet.publicKey,
-        payerTokenAccount: fundingSource,
-        tokenMint: fundingInfo.mint,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        systemProgram: SystemProgram.programId,
-        rent: SYSVAR_RENT_PUBKEY
-      }
+    accounts: {
+      payerTokenAuthority: wallet.publicKey,
+      payerTokenAccount: tokenSource.address
     }
-  )
+  })
 
   /*
   await program.rpc.distributionRelease({
@@ -98,9 +83,8 @@ async function main() {
   });
   */
 
-  console.log(distAccount.toString())
+  const accounts = Distribution.derive(args.seed)
+  console.log(accounts.distribution.toBase58(), accounts.vault.toBase58())
 }
 
-;(async () => {
-  await main()
-})()
+main()
