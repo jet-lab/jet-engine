7d19b240726309013f4924365ebb895a00329a45
Tristyn <tristynstimpson@gmail.com>
2022-03-22 02:54:32 -0700

    Skip preflight

--- src/common/index.ts
+++ src/common/index.ts
@@ -90,7 +90,7 @@ export async function connect<T extends Idl>(programId: PublicKey, provider: Pro
 }
 
 const confirmOptions: ConfirmOptions = {
-  skipPreflight: false,
+  skipPreflight: true,
   commitment: "recent" as Commitment,
   preflightCommitment: "recent"
 }
